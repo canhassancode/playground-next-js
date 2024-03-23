3c66e63a280486bb9530b1b9039fe37309ad0551
Hassan Ali <hassan@oneforge.co.uk>
2024-03-23 09:40:27 +0000

    Updating placeholder messages to test vercel

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const About = () => {
-  return <h1>About</h1>;
+  return (
+    <main className="flex flex-col items-center text-center text-xl m-12 ">
+      <h1 className="font-bold">About Page</h1>
+      <p>Placeholder</p>
+    </main>
+  );
 };
 
 export default About;
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Login = () => {
-  return <h2>Login</h2>;
+  return (
+    <main className="flex flex-col items-center text-center text-xl m-12 ">
+      <h1 className="font-bold">Login Page</h1>
+      <p>Placeholder</p>
+    </main>
+  );
 };
 
 export default Login;
